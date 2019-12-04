77fb9f9856e6faae671593308e28b59cc73d06a5
Richar <rpupo88@gmail.com>
2019-12-04 03:43:26 +0000

    aa

--- docs/precache-manifest.41c9e6f559cbb7cd7590286c042a0b6b.js
+++ docs/precache-manifest.41c9e6f559cbb7cd7590286c042a0b6b.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "f58b27ec2a9dca3cda82e4fbc138341a",
-    "url": "/material-dashboard-react/index.html"
-  },
-  {
-    "revision": "74ecb7e85c088e918c08",
-    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
-  },
-  {
-    "revision": "5155ea661c64c2209e6c",
-    "url": "/material-dashboard-react/static/css/main.6b9dcdb7.chunk.css"
-  },
-  {
-    "revision": "74ecb7e85c088e918c08",
-    "url": "/material-dashboard-react/static/js/2.bd593f7e.chunk.js"
-  },
-  {
-    "revision": "5155ea661c64c2209e6c",
-    "url": "/material-dashboard-react/static/js/main.00a100e4.chunk.js"
-  },
-  {
-    "revision": "03041361d64fb17a9f41",
-    "url": "/material-dashboard-react/static/js/runtime~main.7bd9909c.js"
-  },
-  {
-    "revision": "8880a65c57d7f031579335be153f64a0",
-    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
-  },
-  {
-    "revision": "7805e79572d99b0a116417303b64076b",
-    "url": "/material-dashboard-react/static/media/sidebar-2.7805e795.jpg"
-  }
+    {
+        revision: 'f58b27ec2a9dca3cda82e4fbc138341a',
+        url: 'index.html'
+    },
+    {
+        revision: '74ecb7e85c088e918c08',
+        url: 'static/css/2.d0176e96.chunk.css'
+    },
+    {
+        revision: '5155ea661c64c2209e6c',
+        url: 'static/css/main.6b9dcdb7.chunk.css'
+    },
+    {
+        revision: '74ecb7e85c088e918c08',
+        url: 'static/js/2.bd593f7e.chunk.js'
+    },
+    {
+        revision: '5155ea661c64c2209e6c',
+        url: 'static/js/main.00a100e4.chunk.js'
+    },
+    {
+        revision: '03041361d64fb17a9f41',
+        url: 'static/js/runtime~main.7bd9909c.js'
+    },
+    {
+        revision: '8880a65c57d7f031579335be153f64a0',
+        url: 'static/media/marc.8880a65c.jpg'
+    },
+    {
+        revision: '7805e79572d99b0a116417303b64076b',
+        url: 'static/media/sidebar-2.7805e795.jpg'
+    }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,16 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/material-dashboard-react/precache-manifest.41c9e6f559cbb7cd7590286c042a0b6b.js"
+    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+importScripts('precache-manifest.41c9e6f559cbb7cd7590286c042a0b6b.js');
+
+self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
 workbox.core.clientsClaim();
@@ -33,7 +33,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/material-dashboard-react/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+    workbox.precaching.getCacheKeyForURL('index.html'),
+    {
+        blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/]
+    }
+);
